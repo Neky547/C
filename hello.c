a22815a30e73de1e3e151e70a4104af304fe8c06
Ny Aina Rabarisoa <nekenarab55@gmail.com>
2026-09-04 06:22:18 +0200

    variable declaration

--- hello.c
+++ hello.c
@@ -3,5 +3,9 @@
 int main(void) {
     printf("Hello, World!\n");
     printf("This is my first C program \n");
+
+    // Variable declaration
+    int myNum = 50;
+    printf("The entered value is : %d\n", myNum); 
     return 0;
 }
